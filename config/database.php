8ea0a5d76f0341bff4d7e43281a766b1d92c07b9
nikola-djokovic-web <43373053+nikola-djokovic-web@users.noreply.github.com>
2020-12-26 22:48:14 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '169.254.0.2'),
+            'host' => env('DB_HOST', ''),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'dentast_dentastico'),
-            'username' => env('DB_USERNAME', 'dentast_dentast'),
-            'password' => env('DB_PASSWORD', 'dentastico789'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
